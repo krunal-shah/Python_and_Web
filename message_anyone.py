3f18a88c3370bc131ef8252ebbc41ff666fb2966
krunal-shah <ktgshah@gmail.com>
2016-07-18 02:37:27 +0530

    Update message_anyone.py

--- message_anyone.py
+++ message_anyone.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
